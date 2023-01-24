4caa4b4ebd5d720545a82117e1c00ed50dc943ee
Interborn <dylansafra@icloud.com>
2023-01-24 13:51:38 -0700

    Built out React components for possibility and cta sections and did all styling for desktop and mobile.

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,14 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>
-      CTA
+    <div className='gpt3__cta section__padding' id='cta'>
+      <div className='gpt3__cta-content'>
+        <p>Request Early Access to Get Started</p>
+        <h1>Register today & start exploring the endless possiblities.</h1>
+      </div>
+      <div className='gpt3__cta-button'>
+        <button type='button'>Get Started</button>
+      </div>
     </div>
   )
 }
